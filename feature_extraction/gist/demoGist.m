5436f1000b8f24b2d255aa93e540f01adeacff52
Lby <wqrydqk@umich.edu>
2021-12-06 16:45:51 -0500

    deleted some unused files

--- feature_extraction/gist/demoGist.m
+++ feature_extraction/gist/demoGist.m
@@ -2,7 +2,7 @@ clc
 clear
 
 
-path = 'D:/MyDesktop/Cloud LYM/Fall2021/545/Final_Project/processed_dataset_v4/image/*.jpg';
+path = 'E:\EECS545\processed_dataset_v4\image\*.jpg';
 
 files = dir(path);
 clear param 
@@ -11,18 +11,20 @@ param.orientationsPerScale = [8 8 8 8];
 param.numberBlocks = 4;
 param.fc_prefilt = 4;
 
-result = zeros(16189,512);
-for c = 1:10
+result = zeros(16188,512);
+for c = 1:1:length(files)
     disp(c)
-    file = append(files(c).folder,'\',files(c).name);
+    file = [files(c).folder,'\',files(c).name];
     img2 = imread(file);
+    img2 = imresize(img2, [500, 500]);
 
 
     
     [gist2, param] = LMgist(img2, '', param);
     
     result(c,:) = gist2;
-    save('gist.mat','result')
+    % save('gist.mat','result')
+% save('gist.mat','result')
 
 % 
 %     figure
@@ -34,7 +36,7 @@ for c = 1:10
 %     title('Descriptor')
 
 end
-
+save('gist.mat','result')
 
 
 
