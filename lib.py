56fa8a2c2b849e84133e43405fd01aa7790049a2
Yiming Luo <yimiluo@umich.edu>
2021-12-07 12:04:52 -0500

    LYM-12/7

--- lib.py
+++ lib.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import random_projection
 
-def main():
+def main(feature_directory,tag_directory, weight=False, iteration =20):
     '''
     the main function of this project
     no input
@@ -10,25 +11,28 @@ def main():
     output: 
     four evalution of the test data   
     '''
-    feature_directory = ''
-    tag_directory = ''
-    X = np.load(feature_directory)
-    Y = np.load(tag_directory)
 
+    X = np.load(feature_directory)
+ 
+    Y = np.load(tag_directory)
+    
+    X = random_project(X,0.2)
+    X = X.T
+    Y = Y.T
     np.random.seed(42)
     X = X[:, np.random.permutation(X.shape[1])]
     np.random.seed(42)
     Y = Y[:, np.random.permutation(Y.shape[1])]
-
+    print(X.shape,Y.shape)
     # split train and test data
     n = X.shape[1]
     X_train, X_test = X[:,:int(n*0.8)], X[:,int(n*0.8):]
-    Y_train, Y_test = X[:,:int(n*0.8)], X[:,int(n*0.8):]
+    Y_train, Y_test = Y[:,:int(n*0.8)], Y[:,int(n*0.8):]
 
     # split train and validation data
     n = X_train.shape[1]
     X_train, X_valid = X_train[:,:int(n*0.75)], X_train[:,int(n*0.75):]
-    Y_train, Y_valid = X_train[:,:int(n*0.75)], X_train[:,int(n*0.75):]
+    Y_train, Y_valid = Y_train[:,:int(n*0.75)], Y_train[:,int(n*0.75):]
 
     # initilize matrix
     d = X_train.shape[0]
@@ -40,18 +44,26 @@ def main():
     p = 0.5
 
     # choose one of these two
-    W_new, B_new, total_loss = opt(W, B, X_train, Y_train, p, lamda, gamma)
-    W_new, B_new, total_loss = weight_opt(W, B, X_train, Y_train, p, lamda, gamma)
-
+    if weight == True:
+        W_new, B_new, total_loss = opt(W, B, X_train, Y_train, p, lamda, gamma, iteration)
+    else:
+        W_new, B_new, total_loss = weight_opt(W, B, X_train, Y_train, p, lamda, gamma)
+    print(len(total_loss))
     # plot the loss over iteration
     plt.plot(total_loss)
     plt.show()
 
-    W_new, B_new = bootstrap(W_new, B_new, X_train, Y_train, p, lamda, gamma, X_valid, Y_valid)
-    W_new, B_new = reopt(W_new, B_new, X_train, Y_train, p, lamda, gamma, X_valid, Y_valid)
+    # W_new, B_new = bootstrap(W_new, B_new, X_train, Y_train, p, lamda, gamma, X_valid, Y_valid)
+    # W_new, B_new = reopt(W_new, B_new, X_train, Y_train, p, lamda, gamma, X_valid, Y_valid)
 
     return evalution(W_new, X_test, Y_test)    
 
+
+def random_project(data,eps):
+    a=np.random.RandomState(40)
+    transformer=random_projection.GaussianRandomProjection(random_state=a, eps = eps)
+    data_new=transformer.fit_transform(data)
+    return  data_new
 def cal_PQ(p=0.5,Y=None):
     '''
     calculate matrix P, Q needed for optimization
@@ -63,7 +75,8 @@ def cal_PQ(p=0.5,Y=None):
         Q: t*t-dimensional matrix,  Q = (1 − p)^2YY^T + p(1 − p)δ(YY^T)      
     '''
     P = (1-p)*Y@Y.T
-    Q = (1-p)**2*Y@Y.T+p*(1-p)*np.diagflat((Y@Y.T).diag())
+    # Q = (1-p)**2*Y@Y.T+p*(1-p)*np.diagflat((Y@Y.T).diag())
+    Q = (1-p)**2*Y@Y.T+p*(1-p)*np.diagflat((Y@Y.T).diagonal()) # changed here
 
     return P,Q
 
@@ -84,9 +97,18 @@ def opt_periter(W=None, B=None, X=None, Y=None, P=None, Q=None, lamda=0.1, gamma
         B_new: t*t-dimensional matrix, weights for Y, B_new = (γP+WXY^T)(γQ + YY^T)^-1
     '''
     d = X.shape[0]
+    n = X.shape[1] # change here
     W_new = B@Y@X.T@np.linalg.inv(X@X.T+n*lamda*np.identity(d))
-    B_new = (gamma*P+W@X@Y.T)@np.linalg.inv(gamma*Q+Y@Y.T)
-
+    temp = gamma*Q+Y@Y.T
+    # result = np.sum(Y, axis = 0)
+    # print(result.shape)
+    # for ele in result:
+    #     if ele == 0:
+    #         print('zero row')
+    # print(Q[:10,:10])
+    # print(temp.shape)
+    B_new = (gamma*P+W_new@X@Y.T)@np.linalg.pinv(temp) # change here
+    
     return W_new, B_new
 
 def total_loss(W=None, B=None, X=None, Y=None, P=None, Q=None, lamda=0.1, gamma=0.1):
@@ -123,7 +145,7 @@ def recon_error(B=None,Y=None,P=None,Q=None):
 
     return 1/n*np.trace(B@Q@B.T-2*P@B.T+Y@Y.T)
 
-def opt(W=None, B=None, X=None, Y=None, p=0.5, lamda=0.1, gamma=0.1):
+def opt(W=None, B=None, X=None, Y=None, p=0.5, lamda=0.1, gamma=0.1, iteration = 15):
     '''
     optimize the parametersin W, B and return the total loss in each iteration
     input:
@@ -147,12 +169,14 @@ def opt(W=None, B=None, X=None, Y=None, p=0.5, lamda=0.1, gamma=0.1):
     W_new, B_new = opt_periter(W_new, B_new, X, Y, P, Q, lamda, gamma)
     loss = total_loss(W_new, B_new, X, Y, P, Q, lamda, gamma)
     iter_num = 0
-
-    while abs(loss-loss_list[-1])/loss_list[-1] > 0.05 and iter_num < 19: # stop iteration when loss decreases no larger than 5% or times of iteration over 20
+    
+    while abs(loss-loss_list[-1])/loss_list[-1] > 0.0001 and iter_num < iteration: # stop iteration when loss decreases no larger than 5% or times of iteration over 20
         loss_list.append(loss)
+        print('this is itr: ', iter_num, 'loss: ', loss_list[-1])
         W_new, B_new = opt_periter(W_new, B_new, X, Y, P, Q, lamda, gamma)
         loss = total_loss(W_new, B_new, X, Y, P, Q, lamda, gamma)
         iter_num += 1
+  
     loss_list.append(loss)
 
     return W_new, B_new, loss_list
@@ -168,7 +192,8 @@ def top5_prediction(W=None, X=None, Y=None):
         top5_index: the index of the most five relavent tags for each image
     '''
     Y_predict = W@X
-    diff = np.abs(Y_predict - Y)
+    print(X.shape,W.shape)
+    diff = np.abs(Y_predict-Y )                     
     top5_index = np.argsort(diff,axis=0)
     top5_index = top5_index[:5,:]
 
@@ -198,15 +223,29 @@ def evalution(W=None, X=None, Y=None):
 
     for j in range(t):
         true_index = [i for i, v in enumerate(Y[j,:]) if v == 1] 
+     
         predict_index = [i[1] for i, v in np.ndenumerate(top5_index) if v == j]
         num_of_tp = len(np.intersect1d(true_index, predict_index))
-        precision = num_of_tp/len(predict_index)
+        if len(predict_index) == 0:
+            precision = 0
+        else:
+            precision = num_of_tp/len(predict_index)
         precision_list.append(precision)
-        recall = num_of_tp/len(true_index)
+        if len(true_index) ==0:
+            recall = 0
+        else:
+            recall = num_of_tp/len(true_index)
+
+     
+       
+    
         if recall > 0:
             non_zero_recall += 1
         recall_list.append(recall)
-        f1_score_list.append(2*precision*recall/(precision+recall))
+        if (precision+recall) == 0:
+            f1_score_list.append(0)
+        else:
+            f1_score_list.append(2*precision*recall/(precision+recall))
 
     return precision_list, recall_list, f1_score_list, non_zero_recall
 
@@ -226,7 +265,13 @@ def bootstrap_periter(W=None, B=None, X=None, Y=None, p=0.5, lamda=0.1, gamma=0.
         B: t*t-dimensional matrix, weights for Y
         loss list for each iteration
     '''
-    return opt(W, B, X, B@Y, p, lamda, gamma)
+    Y = B@Y
+    idx_lager_than_1 = (Y > 1)
+    idx_smaller_than_0 = (Y < 0)
+    Y_trucated = Y.copy()
+    Y_trucated[idx_lager_than_1] = 1
+    Y_trucated[idx_smaller_than_0] = 0
+    return opt(W, B, X, Y_trucated, p, lamda, gamma)
 
 def bootstrap(W=None, B=None, X=None, Y=None, p=0.5, lamda=0.1, gamma=0.1, Xhold=None, Yhold=None):
     '''
@@ -246,16 +291,16 @@ def bootstrap(W=None, B=None, X=None, Y=None, p=0.5, lamda=0.1, gamma=0.1, Xhold
         B_new: t*t-dimensional matrix, weights for Y
     '''
     _, _, f1_score_old, _ = evalution(W, Xhold, Yhold)
-    f1_score_old = mean(f1_score_old)
+    f1_score_old = np.mean(f1_score_old)
     W_new, B_new, _ = bootstrap_periter(W, B, X, Y, p, lamda, gamma)
     _, _, f1_score_new, _ = evalution(W_new, Xhold, Yhold)
-    f1_score_new = mean(f1_score_new)
+    f1_score_new = np.mean(f1_score_new)
 
     while abs(f1_score_new - f1_score_old)/f1_score_old > 0.05:
         f1_score_old = f1_score_new
         W_new, B_new, _ = bootstrap_periter(W_new, B_new, X, Y, p, lamda, gamma)
         _, _, f1_score_new, _ = evalution(W_new, Xhold, Yhold)
-        f1_score_new = mean(f1_score_new)
+        f1_score_new = np.mean(f1_score_new)
 
     return W_new, B_new
 
@@ -277,15 +322,16 @@ def reopt_periter(W=None, B=None, X=None, Y=None, p=0.5, lamda=0.1, gamma=0.1, X
         B: t*t-dimensional matrix, weights for Y
     '''
     _, recall, _, _ = evalution(W, Xhold, Yhold)
-    threshold = mean(recall)
+    threshold = np.mean(recall)
     index = [i for i, v in enumerate(recall) if v < threshold]
-    W_new, B_new, _ = opt(W[index,:], B[index,:][:,index], X, Y[index,:], p, lamda, gamma)
-
+    # W_new, B_new, _ = opt(W[index,:], B[index,:][:,index], X, Y[index,:], p, lamda, gamma)
+    W_new, B_new, _ = opt(W[index,:], B[index,:], X, Y, p, lamda, gamma)
     W[index,:] = W_new
-    j = 0
-    for i in index:
-        B[i,index] = B_new[j,:]
-        j += 1
+#    j = 0
+#    for i in index:
+#        B[i,index] = B_new[j,:]
+#        j += 1
+    B[index,:] = B_new
 
     return W, B
 
@@ -307,20 +353,33 @@ def reopt(W=None, B=None, X=None, Y=None, p=0.5, lamda=0.1, gamma=0.1, Xhold=Non
         B_new: t*t-dimensional matrix, weights for Y
     '''
     _, _, f1_score_old, _ = evalution(W, Xhold, Yhold)
-    f1_score_old = mean(f1_score_old)
+    f1_score_old = np.mean(f1_score_old)
     W_new, B_new = reopt_periter(W, B, X, Y, p, lamda, gamma, Xhold, Yhold)
     _, _, f1_score_new, _ = evalution(W_new, Xhold, Yhold)
-    f1_score_new = mean(f1_score_new)
+    f1_score_new = np.mean(f1_score_new)
 
     while abs(f1_score_new - f1_score_old)/f1_score_old > 0.05:
         f1_score_old = f1_score_new
         W_new, B_new, _ = reopt_periter(W_new, B_new, X, Y, p, lamda, gamma, Xhold, Yhold)
         _, _, f1_score_new, _ = evalution(W_new, Xhold, Yhold)
-        f1_score_new = mean(f1_score_new)
+        f1_score_new = np.mean(f1_score_new)
     
     return W_new, B_new
 
-def weight_matrix(Y=None):
+#def weight_matrix(Y=None):
+#    '''
+#    calculate the diagonal weight matrix
+#    input:
+#        Y: t*n-dimensional matrix, partial tags from n images
+#    ouput:
+#        diagonal weight matrix
+#    '''
+#    weight = np.sum(Y==1, axis=1)
+#
+#    return np.diag(np.diag(weight))
+
+
+def weight_matrix(Y=None):   # changed here
     '''
     calculate the diagonal weight matrix
     input:
@@ -328,9 +387,12 @@ def weight_matrix(Y=None):
     ouput:
         diagonal weight matrix
     '''
-    weight = np.sum(Y==1, axis=1)
+    n_w = np.sum(Y, axis=1).reshape(-1,1)
+    c_w = 1/n_w
+    weight_each_example = Y.T @ c_w
 
-    return np.diag(np.diag(weight))
+    return np.diagflat(weight_each_example)
+
 
 def weight_opt_periter(W=None, B=None, X=None, Y=None, P=None, Q=None, lamda=0.1, gamma=0.1, weight=None):
     '''
@@ -350,10 +412,33 @@ def weight_opt_periter(W=None, B=None, X=None, Y=None, P=None, Q=None, lamda=0.1
         B_new: t*t-dimensional matrix, weights for Y, B_new = (γP+WXY^T)(γQ + YY^T)^-1
     '''
     d = X.shape[0]
+    n = X.shape[1] # change here
     W_new = B@Y@weight@X.T@np.linalg.inv(X@weight@X.T+n*lamda*np.identity(d))
-    B_new = (gamma*P+W@X@weight@Y.T)@np.linalg.inv(gamma*Q+Y@weight@Y.T)
+    B_new = (gamma*P+W_new@X@weight@Y.T)@np.linalg.inv(gamma*Q+Y@weight@Y.T)   # changed here
 
     return W_new, B_new
+
+
+
+def total_loss_weighted(W=None, B=None, X=None, Y=None, P=None, Q=None, lamda=0.1, gamma=0.1, weight_mat=None):
+    '''
+    calculate the total loss
+    input:
+        W: t*d-dimensional matrix, weights for X
+        B: t*t-dimensional matrix, weights for Y
+        X: d*n-dimensional matrix, features extraction from n images
+        Y: t*n-dimensional matrix, partial tags from n images
+        lamda: regularization factor for W 
+        P: t*t-dimensional matrix
+        Q: t*t-dimensional matrix
+        gamma: regularization factor for B
+    ouput:
+        total loss
+    '''
+    n = X.shape[1]
+    weight_vec = np.diag(weight_mat).reshape(-1, 1)
+    return 1/n*(np.sum((B@Y-W@X)**2, axis=0)) @ weight_vec + lamda*np.sum(W**2)+gamma*recon_error(B,Y,P,Q)
+
 
 def weight_opt(W=None, B=None, X=None, Y=None, p=0.5, lamda=0.1, gamma=0.1):
     '''
@@ -375,17 +460,28 @@ def weight_opt(W=None, B=None, X=None, Y=None, p=0.5, lamda=0.1, gamma=0.1):
     P, Q = cal_PQ(p,Y)
     weight = weight_matrix(Y)
     W_new, B_new = weight_opt_periter(W, B, X, Y, P, Q, lamda, gamma, weight)
-    loss_list.append(total_loss(W_new, B_new, X, Y, P, Q, lamda, gamma))
-
+    loss_list.append(total_loss_weighted(W_new, B_new, X, Y, P, Q, lamda, gamma, weight))
+    
     W_new, B_new = weight_opt_periter(W_new, B_new, X, Y, P, Q, lamda, gamma, weight)
-    loss = total_loss(W_new, B_new, X, Y, P, Q, lamda, gamma)
+    loss = total_loss_weighted(W_new, B_new, X, Y, P, Q, lamda, gamma, weight)
     iter_num = 0
 
     while abs(loss-loss_list[-1])/loss_list[-1] > 0.05 and iter_num < 19: # stop iteration when loss decreases no larger than 5% or times of iteration over 20
+        print('this is itr: ', iter_num, 'loss: ', loss_list[-1])
         loss_list.append(loss)
         W_new, B_new = weight_opt_periter(W_new, B_new, X, Y, P, Q, lamda, gamma, weight)
-        loss = total_loss(W_new, B_new, X, Y, P, Q, lamda, gamma)
+        loss = total_loss_weighted(W_new, B_new, X, Y, P, Q, lamda, gamma, weight)
         iter_num += 1
+        # print('Iteration: ',iter_num, 'Loss', loss)
     loss_list.append(loss)
 
     return W_new, B_new, loss_list
+
+
+
+if __name__ == "__main__":
+    feature_directory = 'matlab_order_tags/combined_feature_all.npy'
+    tag_directory= 'matlab_order_tags/binary_feature.npy'
+    precision_list, recall_list, f1_score_list, non_zero_recall = main(feature_directory,tag_directory,True, 3)
+    print(sum(precision_list) / len(precision_list), sum(recall_list) / len(recall_list),sum(f1_score_list) / len(f1_score_list), non_zero_recall)
+
